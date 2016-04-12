f6419909898f7d6d7b8be5081a447177ff3fa422
Jbazemore4 <jbazemore4@student.gsu.edu>
2016-04-12 18:54:30 -0400

    Update brandAndVendor.sql

--- brandAndVendor.sql
+++ brandAndVendor.sql
@@ -41,3 +41,29 @@ INSERT INTO tag(id, name) VALUES(null,'Hawkman')
 INSERT INTO tag(id, name) VALUES(null,'Wonder Woman')
 INSERT INTO tag(id, name) VALUES(null,'Supergirl')
 INSERT INTO tag(id, name) VALUES(null,'The Walking Dead')
+
+INSERT INTO brands(id, name) VALUES(null, 'Max Comics (MAX)');
+INSERT INTO brands(id, name) VALUES(null, 'Media Factory');
+INSERT INTO brands(id, name) VALUES(null, 'Michael Hunt Publishing');
+INSERT INTO brands(id, name) VALUES(null, 'Mighty Comics');
+INSERT INTO brands(id, name) VALUES(null, 'Milestone Media');
+INSERT INTO brands(id, name) VALUES(null, 'Mille-Îles');
+INSERT INTO brands(id, name) VALUES(null, 'Millennium Publications');
+INSERT INTO brands(id, name) VALUES(null, 'Milk Shadow Books');
+INSERT INTO brands(id, name) VALUES(null, 'MiniKomix');
+INSERT INTO brands(id, name) VALUES(null, 'Mirage Studios');
+INSERT INTO brands(id, name) VALUES(null, 'Mirror Comics');
+INSERT INTO brands(id, name) VALUES(null, 'M. F. Enterprises');
+INSERT INTO brands(id, name) VALUES(null, 'MLJ Magazines');
+INSERT INTO brands(id, name) VALUES(null, 'Mojo Press');
+INSERT INTO brands(id, name) VALUES(null, 'Moonstone Books');
+INSERT INTO brands(id, name) VALUES(null, 'MOSAIK Steinchen für Steinchen Verlag');
+INSERT INTO brands(id, name) VALUES(null, 'M Press');
+INSERT INTO brands(id, name) VALUES(null, 'MU Press');
+INSERT INTO brands(id, name) VALUES(null, 'MyInkComics.com');
+INSERT INTO brands(id, name) VALUES(null, 'NBM Publishing');
+INSERT INTO brands(id, name) VALUES(null, 'Neko Press');
+INSERT INTO brands(id, name) VALUES(null, 'Standard Comics');
+INSERT INTO brands(id, name) VALUES(null, 'New Century Comics');
+INSERT INTO brands(id, name) VALUES(null, 'New Comics Group');
+INSERT INTO brands(id, name) VALUES(null, 'Square Enix');
